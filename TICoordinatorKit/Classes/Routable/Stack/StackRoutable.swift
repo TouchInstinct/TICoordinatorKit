de83f770f4d33f43b48d44b8c760dba51d46ac02
Ivan Smolin <ivan.smolin@touchin.ru>
2020-12-15 20:26:54 +0300

    add topModule to StackRoutable

--- TICoordinatorKit/Classes/Routable/Stack/StackRoutable.swift
+++ TICoordinatorKit/Classes/Routable/Stack/StackRoutable.swift
@@ -24,7 +24,11 @@ import UIKit
 
 public protocol StackRoutable: ModalRoutable {
 
+    /// Module marked as "head" in the stack
     var headModule: Presentable? { get set }
+
+    /// Module on the top of the stack
+    var topModule: Presentable? { get }
 
     func push(_ module: Presentable?)
     func push(_ module: Presentable?, animated: Bool)
